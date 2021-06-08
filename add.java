0cc8bb28ff4ba2944faa9f5fca5ed3e7b789bae3
sharmaabhijeet <85536574+sharmaabhijeet@users.noreply.github.com>
2021-06-08 08:55:27 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,12 @@
+import java.until.scanner;
+class add
+{
+public static void main(String args[])
+{
+int a=sc.nextInt();
+int b=sc.nextInt();
+int c;
+c=a+b;
+System.out.println(c)
+}
+}
---
+++ for.java
@@ -0,0 +1,11 @@
+class for
+{
+public static void main(String args[])
+{
+int a=10;
+for(a=0;a<10;a++)
+{
+System.out.println(a);
+}
+} 
+}
